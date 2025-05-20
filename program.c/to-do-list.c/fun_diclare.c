a636c8c62e27172b8dee92a141d6f7e57782a5ae
nitinkumargiri <nitingirinitingiri240@gmail.com>
2025-05-20 15:59:58 +0530

    function

---
+++ program.c/to-do-list.c/fun_diclare.c
@@ -0,0 +1,13 @@
+int myFunction(int x, int y);
+
+// The main method
+int main() {
+  int result = myFunction(5, 3); // call the function
+  printf("Result is = %d", result);
+  return 0;
+}
+
+// Function definition
+int myFunction(int x, int y) {
+  return x + y;
+}
